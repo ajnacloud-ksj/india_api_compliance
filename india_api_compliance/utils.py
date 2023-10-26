045e35e2b385d10d0515ab0a80c9e45a40abded8
pnallaajna <pnallaspacerokk@gmail.com>
2023-10-26 02:11:42 +0000

    Added utils.py

---
+++ india_api_compliance/utils.py
@@ -0,0 +1,12 @@
+import frappe
+import logging
+import pyqrcode
+import base64
+from io import BytesIO
+
+@frappe.whitelist()
+def generate_qr_base64(data):
+    qr = pyqrcode.create(data)
+    stream = BytesIO()
+    qr.png(stream, scale=5)
+    return base64.b64encode(stream.getvalue()).decode()
