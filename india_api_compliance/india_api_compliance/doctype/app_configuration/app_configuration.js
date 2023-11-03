a290963967b02229594c386c7fd3c56aa1beb6f2
parameshnalla <sbpraonalla@gmail.com>
2023-11-03 17:27:08 +0530

    Delete india_api_compliance/india_api_compliance/doctype/app_configuration directory

--- india_api_compliance/india_api_compliance/doctype/app_configuration/app_configuration.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Ajna Cloud and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('App Configuration', {
-	// refresh: function(frm) {
-
-	// }
-});
--- india_api_compliance/india_api_compliance/doctype/app_configuration/test_app_configuration.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Ajna Cloud and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestAppConfiguration(FrappeTestCase):
-	pass
